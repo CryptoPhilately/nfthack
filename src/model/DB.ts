16581bbef5ec45e4703f06b637720f7e5e254d52
alexstep <alex.step.kellas@gmail.com>
2021-03-20 16:19:40 +0300

    model fixes

--- src/model/DB.ts
+++ src/model/DB.ts
@@ -5,9 +5,11 @@ import Dexie from 'dexie'
 export interface IGroup {
   id?: number; // Primary key. Optional (autoincremented)
   status: string;
+  ticker: string;
   name: string;
   desc: string;
-  tokenSymbol: string;
+  denomination: number;
+  URI: string; // ipfs link to collection info
 }
 
 // Stamps
@@ -17,8 +19,9 @@ export interface IStamp {
   status: string;
   name: string;
   desc: string;
-  image: string; // ipfs link
   denomination: number;
+  image: string; // ipfs link to image file
+  URI: string; // ipfs link to stamp info
 }
 
 export default class Stamps extends Dexie {
@@ -29,50 +32,11 @@ export default class Stamps extends Dexie {
     super(dbName)
 
     this.version(1).stores({
-      groups: '++id, status, tokenSymbol',
-      stamps: '++id, status, collectionId'
+      groups: '++id, URI, status, ticker',
+      stamps: '++id, URI, status, groupId'
     })
 
     this.groups = this.table('groups')
     this.stamps = this.table('stamps')
   }
 }
-
-/*
-const ObservableKeyValue = new Proxy({
-  account_address: null,
-
-  subscribe (cb:(d:any) => void) {
-    window.addEventListener('changed', event => cb(event.detail))
-    window.addEventListener('storage', ({ key, newValue, oldValue }) => {
-      window.dispatchEvent(new CustomEvent('changed-' + key, { detail: { key, newValue: null, oldValue } }))
-      cb({ key, newValue, oldValue })
-    })
-  },
-  onchange (key:string, cb:(d:any) => void) {
-    window.addEventListener('changed-' + key, event => cb(event.detail))
-  }
-}, {
-  set: (obj, key:string, newValue) => {
-    const oldValue = localStorage[key]
-    localStorage[key] = JSON.stringify(newValue)
-    window.dispatchEvent(new CustomEvent('changed', { detail: { key, newValue, oldValue } }))
-    window.dispatchEvent(new CustomEvent('changed-' + key, { detail: { key, newValue, oldValue } }))
-
-    return true
-  },
-
-  deleteProperty: (obj, key:string) => {
-    const oldValue = JSON.parse(localStorage[key])
-    delete localStorage[key]
-    window.dispatchEvent(new CustomEvent('changed', { detail: { key, newValue: null, oldValue } }))
-    window.dispatchEvent(new CustomEvent('changed-' + key, { detail: { key, newValue: null, oldValue } }))
-
-    return true
-  },
-
-  get: (obj, key:string) => {
-    return obj[key] || JSON.parse(localStorage[key] || 'null')
-  }
-})
-*/
