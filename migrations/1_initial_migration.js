58e49d98a6bff4907e2fb3ed5ab47d31e51eaa10
alexstep <alex.step.kellas@gmail.com>
2021-03-21 14:42:51 +0300

    fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,28 +1,28 @@
-const fs = require("fs");
-const Collections = artifacts.require("Collections");
-const Merkle = artifacts.require("Merkle");
-const Factory = artifacts.require("Factory");
+const fs = require('fs')
+const Collections = artifacts.require('Collections')
+const Merkle = artifacts.require('Merkle')
+const Factory = artifacts.require('Factory')
 
 module.exports = function (deployer) {
   deployer.then(async () => {
-    await deployer.deploy(Merkle);
-    await deployer.deploy(Factory);
+    await deployer.deploy(Merkle)
+    await deployer.deploy(Factory)
     const contract = await deployer.deploy(
       Collections,
-      "CryptoPhilately",
-      "PHIL",
+      'CryptoPhilately',
+      'PHIL',
       Merkle.address,
       Factory.address
-    );
+    )
 
-    const { abi: stampsAbi } = require("../build/contracts/Stamps.json");
-    const { abi: depositoryAbi } = require("../build/contracts/Depository.json");
-    const { abi: collectionsAbi } = require("../build/contracts/Collections.json");
+    const { abi: stampsAbi } = require('../build/contracts/Stamps.json')
+    const { abi: depositoryAbi } = require('../build/contracts/Depository.json')
+    const { abi: collectionsAbi } = require('../build/contracts/Collections.json')
 
-    fs.writeFileSync("./src/config/CollectionsABI.json", JSON.stringify(collectionsAbi));
-    fs.writeFileSync("./src/config/DepositoryABI.json", JSON.stringify(depositoryAbi));
-    fs.writeFileSync("./src/config/StampsABI.json", JSON.stringify(stampsAbi));
+    fs.writeFileSync('./src/config/CollectionsABI.json', JSON.stringify(collectionsAbi, null, ' '))
+    fs.writeFileSync('./src/config/DepositoryABI.json', JSON.stringify(depositoryAbi, null, ' '))
+    fs.writeFileSync('./src/config/StampsABI.json', JSON.stringify(stampsAbi, null, ' '))
 
-    fs.writeFileSync("./src/config/ColletionsLocalAddress.json", JSON.stringify({ address: contract.address }));
-  });
-};
+    fs.writeFileSync('./src/config/ColletionsLocalAddress.json', JSON.stringify({ address: contract.address }))
+  })
+}
